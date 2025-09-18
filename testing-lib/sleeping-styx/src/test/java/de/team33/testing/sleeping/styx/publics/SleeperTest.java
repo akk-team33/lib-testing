f410f7b122aaffa53fe5af6cb44393f99a1d5161
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-18 23:05:05 +0200

    [sleeping-styx] refined implementation and tests

--- testing-lib/sleeping-styx/src/test/java/de/team33/testing/sleeping/styx/publics/SleeperTest.java
+++ testing-lib/sleeping-styx/src/test/java/de/team33/testing/sleeping/styx/publics/SleeperTest.java
@@ -1,27 +1,39 @@
 package de.team33.testing.sleeping.styx.publics;
 
 import de.team33.testing.sleeping.styx.Sleeper;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SleeperTest extends Sleeper {
 
-    @Test
-    final void sleep_MILLIS() {
-        final long t0 = System.currentTimeMillis();
-        sleep(17);
-        final double result = System.currentTimeMillis() - t0;
-        assertEquals(17.0, result, 1.0);
+    @ParameterizedTest
+    @ValueSource(longs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void sleep_MILLIS(final long millis) {
+        final long t0 = System.nanoTime();
+        sleep(millis);
+        final double duration = (System.nanoTime() - t0) / 1000000.0;
+        assertEquals((double) millis, duration, 0.1);
     }
 
-    @Test
-    final void sleep_NANOS() {
+    @ParameterizedTest
+    @ValueSource(longs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void sleep_MICROS(final long micros) {
         final long t0 = System.nanoTime();
-        sleep(29, TimeUnit.NANOSECONDS);
-        final double result = System.nanoTime() - t0;
-        assertEquals(29.0, result, 1.0);
+        sleep(micros, TimeUnit.MICROSECONDS);
+        final double duration = (System.nanoTime() - t0) / 1000.0;
+        assertEquals((double) micros, duration, 100.0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void sleep_NANOS(final long nanos) {
+        final long t0 = System.nanoTime();
+        sleep(nanos, TimeUnit.NANOSECONDS);
+        final double duration = (System.nanoTime() - t0);
+        assertEquals((double) nanos, duration, 100000.0);
     }
 }
