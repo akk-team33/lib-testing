4af43582f1248cf1375acf118b32a65638fbf0ce
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-18 23:05:05 +0200

    [sleeping-styx] refined implementation and tests

--- testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/Sleeper.java
+++ testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/Sleeper.java
@@ -9,9 +9,10 @@ public class Sleeper {
     }
 
     public final void sleep(final long time, final TimeUnit unit) {
-        final long target = System.nanoTime() + unit.toNanos(time);
-        while (System.nanoTime() < target) {
-            // nothing to do
+        final long t0 = System.nanoTime();
+        final long target = unit.toNanos(time);
+        for (long dt = System.nanoTime() - t0; dt < target; dt = System.nanoTime() - t0) {
+            // nothing to do here!
         }
     }
 }
