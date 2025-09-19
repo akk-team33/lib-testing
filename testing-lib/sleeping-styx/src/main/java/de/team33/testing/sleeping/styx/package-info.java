d05470c810eb62b22d840b4056b57a2e3687e81f
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-19 05:24:23 +0200

    [sleeping-styx] added javadoc

--- testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/package-info.java
+++ testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/package-info.java
@@ -1,5 +1,7 @@
 /**
- * Tools and utilities for sleeping while testing.
+ * Provides an alternative to Thread.sleep() for bridging definable periods of time during a test.
+ * <p>
+ * Should only be used with caution when there is no other option than to wait for a defined period of time.
  *
  * @see <a href="https://de.wikipedia.org/wiki/Styx_(Mond)">Styx (Moon)</a>
  */
