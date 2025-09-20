5871d449db5b187856c6855886b6991641437894
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-20 07:01:05 +0200

    Refactored Sleeper -> Bridger

--- testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/package-info.java
+++ testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Provides an alternative to Thread.sleep() for bridging definable periods of time during a test.
+ * Provides methods to bridge short periods of time during a test.
  * <p>
- * Should only be used with caution when there is no other option than to wait for a defined period of time.
+ * Should only be used, and with caution, when there is no other option than to bridge a certain period of time.
  *
  * @see <a href="https://de.wikipedia.org/wiki/Styx_(Mond)">Styx (Moon)</a>
  */
