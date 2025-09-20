72a5d204a9ef00930545c36315723689f8a1fc46
Andreas Kluge-Kaindl <akk-pg@team33.de>
2025-09-20 07:01:05 +0200

    Refactored Sleeper -> Bridger

---
+++ testing-lib/sleeping-styx/src/main/java/de/team33/testing/sleeping/styx/Bridger.java
@@ -0,0 +1,56 @@
+package de.team33.testing.sleeping.styx;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Provides methods to bridge short periods of time during a test.
+ * <p>
+ * <b>Note</b>:
+ * Should only be used, and with caution, when there is no other option than to bridge a certain period of time.
+ * <ul>
+ *     <li>Unlike Thread.sleep(), other threads are not given priority during the period.</li>
+ *     <li>Like Thread.sleep(), it also increases the runtime of a test.</li>
+ *     <li>While it adheres to the specified time periods more accurately than Thread.sleep(),
+ *     it is not absolutely accurate.</li>
+ *     <li>The accuracy ultimately depends on the capabilities of the executing system.</li>
+ * </ul>
+ */
+public class Bridger {
+
+    /**
+     * Bridges at least the specified period of time in <em>milliseconds</em>.
+     * The actually bridged period may be longer than specified, but exceeding it by more than 100 microseconds
+     * should be extremely unlikely.
+     * Ultimately, however, this depends on the executing machine and its current workload.
+     * <p>
+     * The current thread doesn't sleep during the period but remains busy.
+     *
+     * @return The actually bridged period of time in nanoseconds.
+     */
+    public final long bridge(final int milliseconds) {
+        return bridge(milliseconds, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Bridges at least the specified <em>period</em> of time in the given {@link TimeUnit}.
+     * The actually bridged period may be longer than specified, but exceeding it by more than 100 microseconds
+     * should be extremely unlikely.
+     * Ultimately, however, this depends on the executing machine and its current workload.
+     * <p>
+     * The current thread doesn't sleep during the period but remains busy.
+     *
+     * @return The actually bridged period of time in nanoseconds.
+     */
+    public final long bridge(final int period, final TimeUnit unit) {
+        return bridgeNanos(unit.toNanos(period));
+    }
+
+    private long bridgeNanos(final long period) {
+        final long t0 = System.nanoTime();
+        long dt = System.nanoTime() - t0;
+        while (dt < period) {
+            dt = System.nanoTime() - t0;
+        }
+        return dt;
+    }
+}
---
+++ testing-lib/sleeping-styx/src/test/java/de/team33/testing/sleeping/styx/publics/BridgerTest.java
@@ -0,0 +1,33 @@
+package de.team33.testing.sleeping.styx.publics;
+
+import de.team33.testing.sleeping.styx.Bridger;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BridgerTest extends Bridger {
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void bridge_MILLIS(final int millis) {
+        final double result = bridge(millis) / 1000000.0;
+        assertEquals((double) millis, result, 0.1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void bridge_MICROS(final int micros) {
+        final double result = bridge(micros, TimeUnit.MICROSECONDS) / 1000.0;
+        assertEquals((double) micros, result, 100.0);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987})
+    final void bridge_NANOS(final int nanos) {
+        final double result = bridge(nanos, TimeUnit.NANOSECONDS);
+        assertEquals((double) nanos, result, 100000.0);
+    }
+}
